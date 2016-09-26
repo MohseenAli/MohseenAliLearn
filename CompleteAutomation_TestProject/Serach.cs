7e5ed29e76c18b70b9b21ad6819735322236dd49
mohseen a ali <mohseen.it@gmail.com>
2016-09-26 22:58:05 +0530

    Created Search Test Case_v1

---
+++ CompleteAutomation_TestProject/Serach.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+
+namespace CompleteAutomation_TestProject
+{
+    [TestClass]
+    public class TestClass
+    {
+        IWebDriver _driver;
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl("http://www.google.co.in");
+            _driver.Manage().Window.Maximize();
+        }
+        [TestMethod]
+        public void Search() 
+        {
+            _driver.FindElement(By.Name("q")).Clear();
+            _driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            _driver.FindElement(By.Name("btnG")).Click();
+            string Title = _driver.Title;
+            Assert.AreEqual("Selenium - Google Search", Title, "Actual value is same Expected");
+        }
+        [TestCleanup]
+        public void TestTeardown()
+        {
+            _driver.Quit();
+        }
+ 
+    }
+}
